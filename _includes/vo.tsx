148e8428cbce6fcf6805b6eb1ca3bb9fefb9f5f6
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-06-05 10:46:59 +0200

    Better GH Pages deployment

--- _includes/vo.tsx
+++ _includes/vo.tsx
@@ -2,10 +2,10 @@ import { Aside } from "../_components/Aside.tsx";
 import { Preview } from "../_components/Preview.tsx";
 
 export const layout = "index.tsx";
-export default function (data: Lume.Data, helpers: Lume.Helpers) {
+export default function (data: Lume.Data) {
   return (
     <>
-      <Aside vos={data.page.data.vos} data={data} hide />
+      <Aside data={data} hide />
       <main className="flex-1">
         <Preview src={data.content as string} />
       </main>
