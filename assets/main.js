a5ff76e401989a670316846dcf4caac6768fcdee
Zuri Klaschka <pklaschka@users.noreply.github.com>
2024-06-05 11:22:41 +0200

    Improve JS modularity

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,10 @@
-const pdfPreview = document.querySelector("iframe");
+(function fixPdfOverflow() {
+  const pdfPreview = document.querySelector("iframe");
 
-if (pdfPreview) {
-	pdfPreview.onload = function () {
-		pdfPreview.contentWindow.document.body.style.overflow = "hidden";
-	};
-}
+  if (!pdfPreview) {
+    return;
+  }
+  pdfPreview.onload = function () {
+    pdfPreview.contentWindow.document.body.style.overflow = "hidden";
+  };
+})();
